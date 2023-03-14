10ee2744378aec99d66deeea72545efd8d897e5f
radulianoo <octav@raduliano.tech>
2023-03-14 20:57:12 +0200

    Updated child object, changed characters on writing data on database.

--- ChatApp/Resources/DatabaseManager.swift
+++ ChatApp/Resources/DatabaseManager.swift
@@ -25,8 +25,12 @@ final class DatabaseManager {
 extension DatabaseManager {
     
     public func userExists(with email: String, completion: @escaping ((Bool) -> Void)) {
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
         //we add a completion because the function to get data out of the databse is async
-        database.child(email).observeSingleEvent(of: .value) { snapshot in
+        database.child(safeEmail).observeSingleEvent(of: .value) { snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
                 return
@@ -39,7 +43,7 @@ extension DatabaseManager {
     ///Insert new user to database
     public func insertUser(with user: ChatAppUser) {
         //the unique key for users is email
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName,
         ])
@@ -52,5 +56,12 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
+    
     //let profilePictureUrl: String
 }
